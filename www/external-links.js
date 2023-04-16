ae3345479e69afb188e04c67b8d30fd8661b9386
mathieudutour <mathieu@dutour.me>
2023-04-16 10:48:10 +0200

    fix typo

--- www/external-links.js
+++ www/external-links.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", () => {
-  const unify = querySelectorDeep("a[role='option'][href='unifi']");
+  const unify = querySelectorDeep("a[role='option'][href='/unifi']");
   unify.setAttribute(
     "href",
     "https://homeassistant.local:8443/manage/default/dashboard"
   );
   unify.setAttribute("target", "_blank");
 
-  const vacuum = querySelectorDeep("a[role='option'][href='vacuum']");
+  const vacuum = querySelectorDeep("a[role='option'][href='/vacuum']");
   vacuum.setAttribute("href", "http://valetudo-lonelygleefulmink.local/");
   vacuum.setAttribute("target", "_blank");
 });
