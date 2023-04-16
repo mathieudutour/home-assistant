8758ab4b051ad09b6c29522f465a097441164103
mathieudutour <mathieu@dutour.me>
2023-04-16 11:07:06 +0200

    highjack links

--- www/external-links.js
+++ www/external-links.js
@@ -1,5 +1,16 @@
-window.addEventListener("load", () => {
+function highjackLinks() {
   const unify = querySelectorDeep("a[role='option'][href='/unifi']");
+  if (
+    !unify &&
+    !querySelectorDeep(
+      "a[role='option'][href='https://homeassistant.local:8443/manage/default/dashboard']"
+    )
+  ) {
+    setTimeout(() => {
+      highjackLinks();
+    }, 100);
+    return;
+  }
   unify.setAttribute(
     "href",
     "https://homeassistant.local:8443/manage/default/dashboard"
@@ -9,4 +20,8 @@ window.addEventListener("load", () => {
   const vacuum = querySelectorDeep("a[role='option'][href='/vacuum']");
   vacuum.setAttribute("href", "http://valetudo-lonelygleefulmink.local/");
   vacuum.setAttribute("target", "_blank");
+}
+
+window.addEventListener("load", () => {
+  highjackLinks();
 });
